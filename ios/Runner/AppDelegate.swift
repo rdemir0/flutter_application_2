c82e0292c07f761767a48b431982fddbaeaf594c
Ramazan Demir <ramazandemir0@beko.com>
2025-02-28 13:39:51 +0300

    Add / 2.3 - Everything Widget - centered favorite icon

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,32 @@
-import Flutter
 import UIKit
+import Flutter
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
+  private let channelName = "com.example.native_alert"
+
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
+    let controller = window?.rootViewController as! FlutterViewController
+    let channel = FlutterMethodChannel(name: channelName, binaryMessenger: controller.binaryMessenger)
+
+    channel.setMethodCallHandler { (call, result) in
+      if call.method == "showNativeAlert" {
+        self.showNativeAlert()
+        result(nil)
+      } else {
+        result(FlutterMethodNotImplemented)
+      }
+    }
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  private func showNativeAlert() {
+    let alert = UIAlertController(title: "Native Alert", message: "This is a native alert from iOS!", preferredStyle: .alert)
+    alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+    window?.rootViewController?.present(alert, animated: true, completion: nil)
   }
 }
